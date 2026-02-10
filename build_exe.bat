627b14975cf78de0aa446aecb4daf66cde30ecbb
Yalçın Adıyaman <yalcinadiyamannn@gmail.com>
2026-02-10 22:48:56 +0300

    Added screenshots to README

--- build_exe.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Building Discovery Tool...
-pyinstaller --noconfirm --onefile --windowed --name "DiscoveryTool" --add-data "assets;assets" --add-data "locales;locales" --hidden-import "PIL._tkinter_finder" main.py
-echo Build complete! Check the 'dist' folder.
-pause
